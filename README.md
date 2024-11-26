8326f29a566c4a71d0d692de499555ee33b0c772
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2024-11-26 13:16:42 +0700

    v0.1

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# Magisk Module Template
+# 360 Reality Audio Upmix Sony Xperia 5 V Magisk Module
+
+## DISCLAIMER
+- Sony apps and blobs are owned by Sony™.
+- The MIT license specified here is for the Magisk Module only, not for Sony apps and blobs.
+
+## Descriptions
+- Equalizer sound effect ported from Sony Xperia 5 V (pdx237) and integrated as a Magisk Module for all supported and rooted devices with Magisk
+- Global type sound effect
+
+## Sources
+- https://dumps.tadiphone.dev/dumps/sony/pdx237 sssi-user-14-67.1.A.2.229-1-release-keys
+- libmagiskpolicy.so: Kitsune Mask R6687BB53
+
+## Screenshots
+- https://t.me/ryukimodsscreenshots/26
+
+## Requirements
+- arm64-v8a or armeabi-v7a architecture
+- Android 11 (SDK 30) and up
+- Magisk or KernelSU installed
+
+## Installation Guide & Download Link
+- Install this module https://www.pling.com/p// via Magisk app or KernelSU app or Recovery if Magisk installed
+- Install AML Magisk Module https://t.me/ryukinotes/34 only if using any other else audio mod module
+- If you are using KernelSU, you need to disable Unmount Modules by Default in KernelSU app settings
+- Reboot
+- If you are using KernelSU, you need to allow superuser list manually all package name listed in package.txt (and your home launcher app also) (enable show system apps) and reboot afterwards
+- If you are using SUList, you need to allow list manually your home launcher app (enable show system apps) and reboot afterwards
+
+## Known Issues
+- Sample music can't be played (requires ROM codec support)
+- Headphones selection does not have any effect (requires ROM audio hal support)
+- Camera capture to right and left ear can't be detected. Just choose manual method.
+
+## Optionals
+- Global: https://t.me/ryukinotes/35
+- Stream: https://t.me/androidryukimodsdiscussions/26764
+
+## Troubleshootings
+- Global: https://t.me/ryukinotes/34
+
+## Support & Bug Report
+- https://t.me/androidryukimodsdiscussions/2618
+- If you don't do above, issues will be closed immediately
+
+## Credits and Contributors
+- @HuskyDG
+- @ahnet_h
+- https://t.me/viperatmos
+- https://t.me/androidryukimodsdiscussions
+- You can contribute ideas about this Magisk Module here: https://t.me/androidappsportdevelopment
+
+## Sponsors
+- https://t.me/ryukinotes/25
+
+
---
+++ customize.sh
@@ -0,0 +1,446 @@
+# space
+ui_print " "
+
+# var
+UID=`id -u`
+[ ! "$UID" ] && UID=0
+ABILIST=`grep_get_prop ro.product.cpu.abilist`
+if [ ! "$ABILIST" ]; then
+  ABILIST=`grep_get_prop ro.system.product.cpu.abilist`
+fi
+ABILIST32=`grep_get_prop ro.product.cpu.abilist32`
+if [ ! "$ABILIST32" ]; then
+  ABILIST32=`grep_get_prop ro.system.product.cpu.abilist32`
+fi
+if [ ! "$ABILIST32" ]; then
+  [ -f /system/lib/libandroid.so ] && ABILIST32=true
+fi
+
+# log
+if [ "$BOOTMODE" != true ]; then
+  FILE=/data/media/"$UID"/$MODID\_recovery.log
+  ui_print "- Log will be saved at $FILE"
+  exec 2>$FILE
+  ui_print " "
+fi
+
+# optionals
+OPTIONALS=/data/media/"$UID"/optionals.prop
+if [ ! -f $OPTIONALS ]; then
+  touch $OPTIONALS
+fi
+
+# debug
+if [ "`grep_prop debug.log $OPTIONALS`" == 1 ]; then
+  ui_print "- The install log will contain detailed information"
+  set -x
+  ui_print " "
+fi
+
+# recovery
+if [ "$BOOTMODE" != true ]; then
+  MODPATH_UPDATE=`echo $MODPATH | sed 's|modules/|modules_update/|g'`
+  rm -f $MODPATH/update
+  rm -rf $MODPATH_UPDATE
+fi
+
+# run
+. $MODPATH/function.sh
+
+# info
+MODVER=`grep_prop version $MODPATH/module.prop`
+MODVERCODE=`grep_prop versionCode $MODPATH/module.prop`
+ui_print " ID=$MODID"
+ui_print " Version=$MODVER"
+ui_print " VersionCode=$MODVERCODE"
+if [ "$KSU" == true ]; then
+  ui_print " KSUVersion=$KSU_VER"
+  ui_print " KSUVersionCode=$KSU_VER_CODE"
+  ui_print " KSUKernelVersionCode=$KSU_KERNEL_VER_CODE"
+  sed -i 's|#k||g' $MODPATH/post-fs-data.sh
+else
+  ui_print " MagiskVersion=$MAGISK_VER"
+  ui_print " MagiskVersionCode=$MAGISK_VER_CODE"
+fi
+ui_print " "
+
+# architecture
+if [ "$ABILIST" ]; then
+  ui_print "- $ABILIST architecture"
+  ui_print " "
+fi
+NAME=arm64-v8a
+NAME2=armeabi-v7a
+if ! echo "$ABILIST" | grep -q $NAME; then
+  if echo "$ABILIST" | grep -q $NAME2; then
+    rm -rf `find $MODPATH/system -type d -name *64*`
+  else
+    if [ "$BOOTMODE" == true ]; then
+      ui_print "! This ROM doesn't support $NAME nor $NAME2 architecture"
+    else
+      ui_print "! This Recovery doesn't support $NAME nor $NAME2 architecture"
+      ui_print "  Try to install via Magisk app instead"
+    fi
+    abort
+  fi
+fi
+if ! echo "$ABILIST" | grep -q $NAME2; then
+  rm -rf $MODPATH/system*/lib\
+   $MODPATH/system*/vendor/lib
+  if [ "$BOOTMODE" != true ]; then
+    ui_print "! This Recovery doesn't support $NAME2 architecture"
+    ui_print "  Try to install via Magisk app instead"
+    ui_print " "
+  fi
+fi
+
+# sdk
+NUM=30
+if [ "$API" -lt $NUM ]; then
+  ui_print "! Unsupported SDK $API. You have to upgrade your"
+  ui_print "  Android version at least SDK $NUM to use this module."
+  abort
+else
+  ui_print "- SDK $API"
+  ui_print " "
+fi
+
+# recovery
+mount_partitions_in_recovery
+
+# magisk
+magisk_setup
+
+# path
+SYSTEM=`realpath $MIRROR/system`
+VENDOR=`realpath $MIRROR/vendor`
+PRODUCT=`realpath $MIRROR/product`
+SYSTEM_EXT=`realpath $MIRROR/system_ext`
+ODM=`realpath $MIRROR/odm`
+MY_PRODUCT=`realpath $MIRROR/my_product`
+
+# sepolicy
+FILE=$MODPATH/sepolicy.rule
+DES=$MODPATH/sepolicy.pfsd
+if [ "`grep_prop sepolicy.sh $OPTIONALS`" == 1 ]\
+&& [ -f $FILE ]; then
+  mv -f $FILE $DES
+fi
+
+# .aml.sh
+mv -f $MODPATH/aml.sh $MODPATH/.aml.sh
+
+# mod ui
+if [ "`grep_prop mod.ui $OPTIONALS`" == 1 ]; then
+  APP=ThreeSixtyRASettings
+  FILE=/data/media/"$UID"/$APP.apk
+  DIR=`find $MODPATH/system -type d -name $APP`
+  ui_print "- Using modified UI apk..."
+  if [ -f $FILE ]; then
+    cp -f $FILE $DIR
+    chmod 0644 $DIR/$APP.apk
+    ui_print "  Applied"
+  else
+    ui_print "  ! There is no $FILE file."
+    ui_print "    Please place the apk to your internal storage first"
+    ui_print "    and reflash!"
+  fi
+  ui_print " "
+fi
+
+# cleaning
+ui_print "- Cleaning..."
+PKGS=`cat $MODPATH/package.txt`
+if [ "$BOOTMODE" == true ]; then
+  for PKG in $PKGS; do
+    FILE=`find /data/app -name *$PKG*`
+    if [ "$FILE" ]; then
+      RES=`pm uninstall $PKG 2>/dev/null`
+    fi
+  done
+fi
+rm -rf $MODPATH/unused
+remove_sepolicy_rule
+ui_print " "
+
+# function
+cleanup() {
+if [ -f $DIR/uninstall.sh ]; then
+  sh $DIR/uninstall.sh
+fi
+DIR=/data/adb/modules_update/$MODID
+if [ -f $DIR/uninstall.sh ]; then
+  sh $DIR/uninstall.sh
+fi
+}
+
+# cleanup
+DIR=/data/adb/modules/$MODID
+FILE=$DIR/module.prop
+PREVMODNAME=`grep_prop name $FILE`
+if [ "`grep_prop data.cleanup $OPTIONALS`" == 1 ]; then
+  sed -i 's|^data.cleanup=1|data.cleanup=0|g' $OPTIONALS
+  ui_print "- Cleaning-up $MODID data..."
+  cleanup
+  ui_print " "
+elif [ -d $DIR ]\
+&& [ "$PREVMODNAME" != "$MODNAME" ]; then
+  ui_print "- Different module name is detected"
+  ui_print "  Cleaning-up $MODID data..."
+  cleanup
+  ui_print " "
+fi
+
+# function
+permissive_2() {
+sed -i 's|#2||g' $MODPATH/post-fs-data.sh
+}
+permissive() {
+FILE=/sys/fs/selinux/enforce
+SELINUX=`cat $FILE`
+if [ "$SELINUX" == 1 ]; then
+  if ! setenforce 0; then
+    echo 0 > $FILE
+  fi
+  SELINUX=`cat $FILE`
+  if [ "$SELINUX" == 1 ]; then
+    ui_print "  Your device can't be turned to Permissive state."
+    ui_print "  Using Magisk Permissive mode instead."
+    permissive_2
+  else
+    if ! setenforce 1; then
+      echo 1 > $FILE
+    fi
+    sed -i 's|#1||g' $MODPATH/post-fs-data.sh
+  fi
+else
+  sed -i 's|#1||g' $MODPATH/post-fs-data.sh
+fi
+}
+
+# permissive
+if [ "`grep_prop permissive.mode $OPTIONALS`" == 1 ]; then
+  ui_print "- Using device Permissive mode."
+  rm -f $MODPATH/sepolicy.rule
+  permissive
+  ui_print " "
+elif [ "`grep_prop permissive.mode $OPTIONALS`" == 2 ]; then
+  ui_print "- Using Magisk Permissive mode."
+  rm -f $MODPATH/sepolicy.rule
+  permissive_2
+  ui_print " "
+fi
+
+# function
+hide_oat() {
+for APP in $APPS; do
+  REPLACE="$REPLACE
+  `find $MODPATH/system -type d -name $APP | sed "s|$MODPATH||g"`/oat"
+done
+}
+replace_dir() {
+if [ -d $DIR ] && [ ! -d $MODPATH$MODDIR ]; then
+  REPLACE="$REPLACE $MODDIR"
+fi
+}
+hide_app() {
+for APP in $APPS; do
+  DIR=$SYSTEM/app/$APP
+  MODDIR=/system/app/$APP
+  replace_dir
+  DIR=$SYSTEM/priv-app/$APP
+  MODDIR=/system/priv-app/$APP
+  replace_dir
+  DIR=$PRODUCT/app/$APP
+  MODDIR=/system/product/app/$APP
+  replace_dir
+  DIR=$PRODUCT/priv-app/$APP
+  MODDIR=/system/product/priv-app/$APP
+  replace_dir
+  DIR=$MY_PRODUCT/app/$APP
+  MODDIR=/system/product/app/$APP
+  replace_dir
+  DIR=$MY_PRODUCT/priv-app/$APP
+  MODDIR=/system/product/priv-app/$APP
+  replace_dir
+  DIR=$PRODUCT/preinstall/$APP
+  MODDIR=/system/product/preinstall/$APP
+  replace_dir
+  DIR=$SYSTEM_EXT/app/$APP
+  MODDIR=/system/system_ext/app/$APP
+  replace_dir
+  DIR=$SYSTEM_EXT/priv-app/$APP
+  MODDIR=/system/system_ext/priv-app/$APP
+  replace_dir
+  DIR=$VENDOR/app/$APP
+  MODDIR=/system/vendor/app/$APP
+  replace_dir
+  DIR=$VENDOR/euclid/product/app/$APP
+  MODDIR=/system/vendor/euclid/product/app/$APP
+  replace_dir
+done
+}
+
+# hide
+APPS="`ls $MODPATH/system/priv-app`
+      `ls $MODPATH/system/app`"
+hide_oat
+APPS="$APPS MusicFX"
+hide_app
+
+# mono
+if [ "`grep_prop tsra.mono $OPTIONALS`" == 1 ]; then
+  ui_print "- Using mono speaker mode"
+  cp -rf $MODPATH/system_mono/* $MODPATH/system
+  rm -rf $MODPATH/system_mono
+  ui_print " "
+fi
+
+# stream mode
+FILE=$MODPATH/.aml.sh
+PROP=`grep_prop stream.mode $OPTIONALS`
+if echo "$PROP" | grep -q m; then
+  ui_print "- Activating music stream..."
+  sed -i 's|#m||g' $FILE
+  sed -i 's|music_stream false|music_stream true|g' $MODPATH/service.sh
+  ui_print "  The sound effect will always be enabled"
+  ui_print "  and cannot be disabled by on/off togglers"
+  ui_print " "
+else
+  APPS=AudioFX
+  hide_app
+fi
+if echo "$PROP" | grep -q r; then
+  ui_print "- Activating ring stream..."
+  sed -i 's|#r||g' $FILE
+  ui_print " "
+fi
+if echo "$PROP" | grep -q a; then
+  ui_print "- Activating alarm stream..."
+  sed -i 's|#a||g' $FILE
+  ui_print " "
+fi
+if echo "$PROP" | grep -q s; then
+  ui_print "- Activating system stream..."
+  sed -i 's|#s||g' $FILE
+  ui_print " "
+fi
+if echo "$PROP" | grep -q v; then
+  ui_print "- Activating voice_call stream..."
+  sed -i 's|#v||g' $FILE
+  ui_print " "
+fi
+if echo "$PROP" | grep -q n; then
+  ui_print "- Activating notification stream..."
+  sed -i 's|#n||g' $FILE
+  ui_print " "
+fi
+if echo "$PROP" | grep -q b; then
+  ui_print "- Activating bluetooth_sco stream..."
+  sed -i 's|#b||g' $FILE
+  ui_print " "
+fi
+if echo "$PROP" | grep -q f; then
+  ui_print "- Activating dtmf stream..."
+  sed -i 's|#f||g' $FILE
+  ui_print " "
+fi
+if echo "$PROP" | grep -q e; then
+  ui_print "- Activating enforced_audible stream..."
+  sed -i 's|#e||g' $FILE
+  ui_print " "
+fi
+if echo "$PROP" | grep -q y; then
+  ui_print "- Activating accessibility stream..."
+  sed -i 's|#y||g' $FILE
+  ui_print " "
+fi
+if echo "$PROP" | grep -q t; then
+  ui_print "- Activating tts stream..."
+  sed -i 's|#t||g' $FILE
+  ui_print " "
+fi
+if echo "$PROP" | grep -q i; then
+  ui_print "- Activating assistant stream..."
+  sed -i 's|#i||g' $FILE
+  ui_print " "
+fi
+if echo "$PROP" | grep -q c; then
+  ui_print "- Activating call_assistant stream..."
+  sed -i 's|#c||g' $FILE
+  ui_print " "
+fi
+if [ "`grep_prop tsra.game $OPTIONALS`" != 0 ]; then
+  sed -i 's|#p||g' $FILE
+  sed -i 's|#g||g' $FILE
+else
+  ui_print "- Does not use 360 Reality Audio game patch &"
+  ui_print "  rerouting stream"
+  ui_print " "
+fi
+
+# function
+rename_file() {
+if [ -f $FILE ]; then
+  ui_print "- Renaming"
+  ui_print "$FILE"
+  ui_print "  to"
+  ui_print "$MODFILE"
+  mv -f $FILE $MODFILE
+  ui_print " "
+fi
+}
+change_name() {
+if grep -q $NAME $FILE; then
+  ui_print "- Changing $NAME to $NAME2 at"
+  ui_print "$FILE"
+  ui_print "  Please wait..."
+  sed -i "s|$NAME|$NAME2|g" $FILE
+  ui_print " "
+fi
+}
+
+# audio rotation
+FILE=$MODPATH/service.sh
+if [ "`grep_prop audio.rotation $OPTIONALS`" == 1 ]; then
+  ui_print "- Enables ro.audio.monitorRotation=true"
+  sed -i '1i\
+resetprop -n ro.audio.monitorRotation true\
+resetprop -n ro.audio.monitorWindowRotation true' $FILE
+  ui_print " "
+fi
+
+# raw
+FILE=$MODPATH/.aml.sh
+if [ "`grep_prop disable.raw $OPTIONALS`" == 0 ]; then
+  ui_print "- Does not disable Ultra Low Latency (Raw) playback"
+  ui_print " "
+else
+  sed -i 's|#u||g' $FILE
+fi
+
+# run
+. $MODPATH/copy.sh
+. $MODPATH/.aml.sh
+
+# overlay
+if [ -d $MODPATH/system/product/overlay ]\
+&& [ ! -d /product/overlay ]; then
+  ui_print "- Using /vendor/overlay/ instead of /product/overlay/"
+  cp -rf $MODPATH/system/product/overlay $MODPATH/system/vendor
+  rm -rf $MODPATH/system/product/overlay
+  ui_print " "
+fi
+
+# unmount
+unmount_mirror
+
+
+
+
+
+
+
+
+
+
